60c681fe5f639e64435b59c15ec6c45641680c93
tesfayealex <tesfayealemayehu27@gmail.com>
2022-07-23 18:49:53 +0300

    added some dbt docs

---
+++ dbt/target/compiled/datawarehouse/models/example/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from "trial"."trial"."my_first_dbt_model"
+where id is null
+
+
---
+++ dbt/target/compiled/datawarehouse/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from "trial"."trial"."my_second_dbt_model"
+where id is null
+
+
