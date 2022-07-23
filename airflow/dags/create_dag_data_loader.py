7473f03ed9bb39ddbb05657ca6e8e6d69b6b60dd
tesfayealex <tesfayealemayehu27@gmail.com>
2022-07-23 23:10:05 +0300

    added raw transformation codes

--- airflow/dags/create_dag_data_loader.py
+++ airflow/dags/create_dag_data_loader.py
@@ -12,6 +12,22 @@ default_args={
     'retry_delay':timedelta(minutes=2)
 }
 
+def modify_raw_data():
+    updated_lines=""
+    with open('/home/hp/Downloads/dataset.csv', 'r', encoding='ISO-8859-1') as f:
+            lines = f.readlines()
+            for index , line in enumerate(lines):
+                if(index == 0):
+                    data = line 
+                each_line = line.split(';')
+                if index != 0:
+                    updated_lines += ";".join(each_line[0:10]) + ";" + "_".join(each_line[10:])
+                else:
+                    updated_lines += ";".join(each_line[:len(each_line)-1]) + ";" + "time" + ";" + "other_data" + "\n" 
+    with open('/home/hp/Downloads/transformed_data.csv', "w") as f:
+        f.writelines(updated_lines)
+
+
 with DAG(
     dag_id='load_data',
     default_args=default_args,
